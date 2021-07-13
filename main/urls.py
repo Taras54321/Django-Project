93a463537e357300c90acaebf1a55b818f4e08ee
Taras54321 <tarasenkoam54321@gmail.com>
2021-07-13 16:18:22 +0300

    added main

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('about_us', views.about_us, name='about_us'),
+    path('create', views.create, name='create'),
+]
